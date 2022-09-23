92228f880744bfa28e3bd0efd9bc463dadb838ae
gajalakshmiyarramsetti <110374769+gajalakshmiyarramsetti@users.noreply.github.com>
2022-09-23 21:01:00 +0530

    Add files via upload

---
+++ sumnaturalnum.java
@@ -0,0 +1,10 @@
+public class sumnaturalnum{
+    public static void main(String[] args) {
+        int num=50,sum=0;
+        for(int i =1;i<=num;++i)
+        {
+            sum+=i;
+        }
+        System.out.println(" sum of all natural number from 1 to 50: "+ sum);
+    }
+}
